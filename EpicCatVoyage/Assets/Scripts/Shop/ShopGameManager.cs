813a9fe21237447d434a09f97914b1bcf22b8b0a
minyeong <ninja2319@naver.com>
2022-11-22 16:41:23 +0900

    돈 data

--- EpicCatVoyage/Assets/Scripts/Shop/ShopGameManager.cs
+++ EpicCatVoyage/Assets/Scripts/Shop/ShopGameManager.cs
@@ -20,15 +20,29 @@ public class ShopItem
     
 }
 
+[System.Serializable]
+public class Cat
+{
+    public Cat(string _Type, string _Money, string _Hungry)
+    {
+        Type = _Type; Money = _Money;  Hungry = _Hungry;
+    }
+
+    public string Type, Money, Hungry;
+}
+
 
 
 
 public class ShopGameManager : MonoBehaviour
 {
     public TextAsset ShopItemDatabase;
+    public TextAsset CatData;
+    public List<Cat> CatList;
     public List<ShopItem> AllShopItemList, ShopItemList, CurItemList, MyItemList;
     public string curType = "Snack";
     public GameObject[] Slot;
+    public GameObject[] Money; 
     public Image[] TabImage, ShopItemImage;
     public Sprite TabIdleSprite, TabSelectSprite;
     public Sprite[] ShopItemSprite;
@@ -38,6 +52,7 @@ public class ShopGameManager : MonoBehaviour
     IEnumerator PointerCoroutine;
     RectTransform ExplainRect;
     string shopfilePath;
+    string catfilePath;
     public GameObject buyBtn;
     public GameObject cancleBtn;
 
@@ -53,10 +68,25 @@ public class ShopGameManager : MonoBehaviour
 
             AllShopItemList.Add(new ShopItem(row[0], row[1], row[2], row[3], row[4] == "TRUE"));
         }
-        shopfilePath = Application.persistentDataPath + "/ShopItemText.txt";
+        //shopfilePath = Application.persistentDataPath + "/ShopItemText.txt";
         //Save();
         Load();
         ExplainRect = ExplainPanel.GetComponent<RectTransform>();
+
+        // 고양이 정보(돈) 불러오기
+        string[] cat = CatData.text.Substring(0, CatData.text.Length - 1).Split('\n');
+        for(int i = 0; i < cat.Length; i++)
+        {
+            string[] cat_row = cat[i].Split('\t');
+
+            CatList.Add(new Cat(cat_row[0], cat_row[1], cat_row[2]));
+        }
+        //catfilePath = Application.persistentDataPath + "/CatData.txt";
+        Save();
+        //Load();
+
+        // 돈 출력하기
+
     }
 
     private void Update()
@@ -201,6 +231,9 @@ public class ShopGameManager : MonoBehaviour
         string jdata_my = JsonConvert.SerializeObject(MyItemList);
         File.WriteAllText(Application.dataPath + "/JSON_files/MyItemText.txt", jdata_my);
         TabClick(curType);
+
+        string jdata_cat = JsonConvert.SerializeObject(CatList);
+        File.WriteAllText(Application.dataPath + "/JSON_files/CatData.txt", jdata_cat);
     }
 
     void Load()
@@ -219,6 +252,8 @@ public class ShopGameManager : MonoBehaviour
         string jdata_my = File.ReadAllText(Application.dataPath + "/JSON_files/MyItemText.txt");
         MyItemList = JsonConvert.DeserializeObject<List<ShopItem>>(jdata_my);
 
+        /*string jdata_cat = File.ReadAllText(Application.dataPath + "/JSON_files/CatData.txt");
+        CatList = JsonConvert.DeserializeObject<List<Cat>>(jdata_cat);*/
 
         TabClick(curType);
     }
