5096b7a8fa9c3e4811a2303ca0ad0689e8be369c
minyeong <ninja2319@naver.com>
2022-12-10 00:02:15 +0900

    상점 이미지 오류 수정

---
+++ EpicCatVoyage/Assets/StoreGameManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class StoreItem
+{
+    public StoreItem(string _Type, string _Name, string _Explain, string _Price, string _Number, bool _isUsing)
+    {
+        Type = _Type; Name = _Name; Explain = _Explain; Price = _Price; Number = _Number; isUsing = _isUsing;
+    }
+    public string Type, Name, Explain, Price, Number;
+    public bool isUsing;
+}
+
+public class StoreGameManager : MonoBehaviour
+{
+
+    public TextAsset ItemDatabase;
+    public List<StoreItem> AllItemList, StoreItemList, CurItemList;
+    public string curType = "Snack";
+    public GameObject[] Slot;
+    public Image[] TabImage, ItemImage;
+    public Sprite TabIdleSprite, TabSelectSprite;
+    public Sprite[] ItemSprite;
+
+    void Start()
+    {
+        // 상점 아이템 리스트 불러오기
+        string[] line = ItemDatabase.text.Substring(0, ItemDatabase.text.Length - 1).Split('\n');
+        for(int i =0; i< line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+
+            AllItemList.Add(new StoreItem(row[0], row[1], row[2], row[3], row[4], row[5] == "TRUE"));
+        }
+
+
+
+        Load();
+    }
+
+    public void TabClick(string tabName)
+    {
+        // 현재 아이템 리스트에 클릭한 타입만 추가
+        curType = tabName;
+        CurItemList = AllItemList.FindAll(x => x.Type == tabName);
+
+        // 슬롯과 텍스트 보이기
+        for(int i =0; i< Slot.Length; i++)
+        {
+            bool isExist = i < CurItemList.Count;
+            Slot[i].SetActive(isExist);
+            Slot[i].GetComponentInChildren<Text>().text = isExist ? CurItemList[i].Name : "";
+            Slot[i].transform.GetChild(2).GetComponentInChildren<Text>().text = isExist ? CurItemList[i].Price : "";
+
+            if (isExist)
+            {
+                ItemImage[i].sprite = ItemSprite[AllItemList.FindIndex(x => x.Name == CurItemList[i].Name)];
+            }
+
+
+        }
+
+        // 탭 이미지
+        int tabNum = 0;
+        switch (tabName)
+        {
+            case "Snack": tabNum = 0; break;
+            case "Home": tabNum = 1; break;
+        }
+        for(int i=0; i < TabImage.Length; i++)
+        {
+            TabImage[i].sprite = i == tabNum ? TabSelectSprite : TabIdleSprite;
+        }
+    }
+
+    void Save()
+    {
+        // 상점 전체리스트 저장
+        string jdata = JsonConvert.SerializeObject(AllItemList);
+        File.WriteAllText(Application.dataPath + "/JSON_files/StoreItemText.txt", jdata);
+    }
+
+    void Load()
+    {
+        string jdata = File.ReadAllText(Application.dataPath + "/JSON_files/StoreItemText.txt");
+        StoreItemList = JsonConvert.DeserializeObject<List<StoreItem>>(jdata);
+
+        TabClick(curType);
+    }
+
+}
