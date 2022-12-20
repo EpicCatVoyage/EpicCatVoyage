789a53ab57b593a9fdd8e212bbfce492aacb3f4c
choiminsun <backwonand@sookmyung.ac.kr>
2022-12-21 08:46:55 +0900

    Village 대화시스템 만듦.

--- EpicCatVoyage/Assets/Scripts/Village/Dialog/Dialog.cs
+++ EpicCatVoyage/Assets/Scripts/Village/Dialog/Dialog.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable] //인스펙터 뷰에 노출
-
 public class Dialog //그냥 하나의 자료형...?입니다
 {
     [TextArea(3, 10)]
@@ -13,6 +12,7 @@ public class Dialog //그냥 하나의 자료형...?입니다
 
     //사용법은 트리거에 있습니다.
 }
+[System.Serializable]
 public class Dialog2
 {
     public Dialog[] dia; // = new Dialog[2];
