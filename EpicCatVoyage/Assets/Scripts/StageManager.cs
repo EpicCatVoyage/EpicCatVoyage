86f1a4216cb9a2455cb53e90d038b0161770741f
choiminsun <backwonand@sookmyung.ac.kr>
2022-12-20 19:08:09 +0900

    Stage Choice씬 제작

---
+++ EpicCatVoyage/Assets/Scripts/StageManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    static int stage = 0;
+
+    public void setStage1() { stage = 1; }
+    public void setStage2() { stage = 2; }
+    public void setStage3() { stage = 3; } 
+    public void setStage4() { stage = 4; }
+}
